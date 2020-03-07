61f9bbf06cdc3f0507817623eb1e296ba5b7687b
sagar jadav ramesh rao <srame070@uottawa.ca>
2020-03-07 18:55:35 -0500

    initial commit

---
+++ PayCompute.Services/IEmployeeService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Paycompute.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+namespace Paycompute.Services
+{
+    public interface IEmployeeService
+    {
+        Task CreateAsync(Employee newEmployee);
+        Employee GetById(int employeeId);
+        Task UpdateAsync(Employee employee);
+        Task UpdateAsync(int id);
+        Task Delete(int employeeId);
+        decimal UnionFees(int id);
+        decimal StudentLoanRepaymentAmount(int id, decimal totalAmount);
+        IEnumerable<Employee> GetAll();
+        IEnumerable<SelectListItem> GetAllEmployeesForPayroll();
+
+    }
+}
